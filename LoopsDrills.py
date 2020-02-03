07113109e4ae83a46a1b4202a896a5b9e7198550
anna463 <57601829+anna463@users.noreply.github.com>
2020-02-02 19:11:50 -0600

    Add files via upload

---
+++ LoopsDrills.py
@@ -0,0 +1,72 @@
+'''
+Created on Feb 2, 2020
+
+@author: ITAUser
+'''
+
+'''
+For this assignment you should read the task, then below the task do what it asks you to do
+based on what the task tells you do first.
+EXAMPLE TASK:
+'''
+#EX) Declare a variable set to 3. Make a while loop that prints the variable 
+#    you just created and decrements the variable by one each time through
+#    the loop. Meanwhile, make the loop run until the variable you created 
+#    equals 0.
+i = 3;
+while i > 0:
+    print(i)
+    i -= 1
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+'''While Loops'''
+#1) Declare a variable set to 4. Make a while loop that prints the variable 
+#    you just created and decrements the variable by one each time through
+#    the loop. Meanwhile, make the loop run until the variable you created 
+#    equals 1.
+i = 4
+while i > 1:
+    print (i)
+    i -= 1
+
+
+#2) Declare a variable set to 14. Make a while loop that prints the variable 
+#    you just created and increments the variable by one each time through
+#    the loop. Meanwhile, make the loop run until the variable you created 
+#    equals 20.
+i = 14
+while i > 20:
+    print (i)
+    i -= 20
+#3) Declare a variable set to 55. Make a while loop that prints the variable 
+#   you just created. Then make an if statement that makes the loop break when
+#   the variable is equal to 50. 
+i = 55
+while i > 50:
+    print (i)
+    i -= 50
+
+
+'''For Loops'''
+#4) Create a list named sports. Put three sports into the list. Create
+#   a for loop that prints each sport in the list
+Sports = ["hockey","soccer","tennis"]
+for x in Sports:
+    print (x)
+#5) Create a for loop that loops through each letter in a string of one of your
+#   favorite songs. Each iteration should print should a letter of the word. 
+x = "twinkletwinkle"
+while (True):
+    print (x)
+    x = x + 3
+    if (x >= 10):
+
+
+
